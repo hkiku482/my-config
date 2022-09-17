83d7d0abd58afadc19d0bc2153cc9d4fad166854
hkiku482 <80496940+hkiku482@users.noreply.github.com>
2022-09-17 09:44:11 +0900

    Update install-arch.sh

--- install-arch.sh
+++ install-arch.sh
@@ -22,6 +22,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S git wget openssh adobe-source-han-sans-jp-fonts noto-fonts-cjk
 pacman -S gnome gnome-extra fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-im
+
 echo "GTK_IM_MODULE=fcitx5" >> /etc/environment
 echo "QT_IM_MODULE=fcitx5" >> /etc/environment
 echo "XMODIFIERS=@im=fcitx5" >> /etc/environment
@@ -29,8 +30,16 @@ echo "DefaultIMModule=fcitx5" >> /etc/environment
 
 systemctl enable gdm
 
-echo "------- edit some files and type commands -------"
-echo "vim /etc/locale.gen"
-echo "locale.gen"
-echo "visudo"
-echo "------- edit some files and type commands -------"
+echo "please edit /etc/locale.gen"
+echo "press enter key to continue..."
+read
+vim /etc/locale.gen
+locale.gen
+
+echo -n "Do you want to edit sodo (visudo)? [Y/n]"
+read VISUDO
+case $VISUDO in
+    "" | [Yy]* ) visudo ;;
+    * ) echo "skipped visudo" ;;
+esac
+unset VISUDO
