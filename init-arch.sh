7a45ebebd9383e9c09f3ce4ec60897c62aac7c76
hkiku482 <80496940+hkiku482@users.noreply.github.com>
2022-11-04 01:58:22 +0900

    Update init-arch.sh

--- init-arch.sh
+++ init-arch.sh
@@ -1,4 +1,6 @@
-GOARCHI="go1.18.3.linux-amd64.tar.gz"
+# this script can run Arch linux based distros (Manjaro, EndoavourOS etc)
+
+GOARCHI="go1.19.3.linux-amd64.tar.gz"
 
 sudo pacman -Syy
 sudo pacman -S vim zsh git wget openssh adobe-source-han-sans-jp-fonts noto-fonts-cjk fcitx5 fcitx5-mozc fcitx5-configtool fcitx5-im
@@ -26,6 +28,15 @@ EOS
    * ) echo "skipped github setting";;
 esac
 
+echo -n "do you want to set up docker? [Y/n]"
+read DOCKER_A
+case $DOCKER_A in
+    "" | [Yy]* )
+    sudo pacman -S docker docker-compose
+    sudo gpasswd -a $USER docker
+    ;;
+   * ) echo "skipped docker setting";;
+esac
 
 echo -n "do you want to get cargo(rustc)? [Y/n]"
 read RUSTC_A
@@ -51,17 +62,181 @@ case $GITHUB_A in
     "" | [Yy]* ) cat ~/.ssh/github.pub
 esac
 
-# mkdir -p ~/workspaces
-# mkdir -p ~/Downloads/AUR
+git clone https://github.com/powerline/fonts.git ./powerlinefonts
+./powerlinefonts/install.sh
+rm -rf powerlinefonts
 
-# cd ~/Downloads/AUR
-# git clone https://aur.archlinux.org/google-chrome.git
-# cd google-chrome
-# makepkg -si
-# cd ~/Downloads/AUR
-# git clone https://aur.archlinux.org/visual-studio-code-bin.git
-# cd visual-studio-code-bin
-# makepkg -si
-# cd
+# source https://raw.githubusercontent.com/agnoster/agnoster-zsh-theme/master/agnoster.zsh-theme
+sudo cat << EOS > /usr/share/zsh/functions/Prompts/prompt_agnoster_setup
+typeset -aHg AGNOSTER_PROMPT_SEGMENTS=(
+    prompt_status
+    prompt_context
+    prompt_virtualenv
+    prompt_dir
+    prompt_git
+    prompt_end
+)
 
-unset GITHUB_A GITNAME GITEMAIL RUSTC_A GOARCHI
+CURRENT_BG='NONE'
+if [[ -z "$PRIMARY_FG" ]]; then
+        PRIMARY_FG=black
+fi
+
+SEGMENT_SEPARATOR="\ue0b0"
+PLUSMINUS="\u00b1"
+BRANCH="\ue0a0"
+DETACHED="\u27a6"
+CROSS="\u2718"
+LIGHTNING="\u26a1"
+GEAR="\u2699"
+
+prompt_segment() {
+  local bg fg
+  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+    print -n "%{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%}"
+  else
+    print -n "%{$bg%}%{$fg%}"
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && print -n $3
+}
+
+prompt_end() {
+  if [[ -n $CURRENT_BG ]]; then
+    print -n "%{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
+  else
+    print -n "%{%k%}"
+  fi
+  print -n "%{%f%}"
+  CURRENT_BG=''
+}
+
+prompt_context() {
+  local user=`whoami`
+
+  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CONNECTION" ]]; then
+    prompt_segment $PRIMARY_FG default " %(!.%{%F{yellow}%}.)$user@%m "
+  fi
+}
+
+prompt_git() {
+  local color ref
+  is_dirty() {
+    test -n "$(git status --porcelain --ignore-submodules)"
+  }
+  ref="$vcs_info_msg_0_"
+  if [[ -n "$ref" ]]; then
+    if is_dirty; then
+      color=yellow
+      ref="${ref} $PLUSMINUS"
+    else
+      color=green
+      ref="${ref} "
+    fi
+    if [[ "${ref/.../}" == "$ref" ]]; then
+      ref="$BRANCH $ref"
+    else
+      ref="$DETACHED ${ref/.../}"
+    fi
+    prompt_segment $color $PRIMARY_FG
+    print -n " $ref"
+  fi
+}
+
+prompt_dir() {
+  prompt_segment blue $PRIMARY_FG ' %~ '
+}
+
+prompt_status() {
+  local symbols
+  symbols=()
+  [[ $RETVAL -ne 0 ]] && symbols+="%{%F{red}%}$CROSS"
+  [[ $UID -eq 0 ]] && symbols+="%{%F{yellow}%}$LIGHTNING"
+  [[ $(jobs -l | wc -l) -gt 0 ]] && symbols+="%{%F{cyan}%}$GEAR"
+
+  [[ -n "$symbols" ]] && prompt_segment $PRIMARY_FG default " $symbols "
+}
+
+prompt_virtualenv() {
+  if [[ -n $VIRTUAL_ENV ]]; then
+    color=cyan
+    prompt_segment $color $PRIMARY_FG
+    print -Pn " $(basename $VIRTUAL_ENV) "
+  fi
+}
+
+prompt_agnoster_main() {
+  RETVAL=$?
+  CURRENT_BG='NONE'
+  for prompt_segment in "${AGNOSTER_PROMPT_SEGMENTS[@]}"; do
+    [[ -n $prompt_segment ]] && $prompt_segment
+  done
+}
+
+prompt_agnoster_precmd() {
+  vcs_info
+  PROMPT='%{%f%b%k%}$(prompt_agnoster_main) '
+}
+
+prompt_agnoster_setup() {
+  autoload -Uz add-zsh-hook
+  autoload -Uz vcs_info
+
+  prompt_opts=(cr subst percent)
+
+  add-zsh-hook precmd prompt_agnoster_precmd
+
+  zstyle ':vcs_info:*' enable git
+  zstyle ':vcs_info:*' check-for-changes false
+  zstyle ':vcs_info:git*' formats '%b'
+  zstyle ':vcs_info:git*' actionformats '%b (%a)'
+}
+
+prompt_agnoster_setup "$@"
+EOS
+
+cat << EOS > ~/.zshrc
+# Set up the prompt
+
+autoload -Uz promptinit
+promptinit
+prompt agnoster
+
+setopt histignorealldups sharehistory
+
+# Use emacs keybindings even if our EDITOR is set to vi
+bindkey -e
+
+# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+
+# Use modern completion system
+autoload -Uz compinit
+compinit
+
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%szstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
+alias ll='ls -ahlvF'
+EOS
+
+chsh -s /bin/zsh
+
