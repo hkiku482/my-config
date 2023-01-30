2da1aab635d8c3e5a43a33cb4a27b50f45eb02f4
hkiku482 <h.kikuchi482@gmail.com>
2023-01-31 04:45:37 +0900

    a

---
+++ installer/use-i3.sh
@@ -0,0 +1,4 @@
+sudo pacman -S xorg-server xorg-xinit --needed
+sudo pacman -S lightdm lightdm-webkit2-greeter
+sudo pacman -S i3-wm rofi i3blocks conky nitrogen xcompmgr
+sudo pacman -S terminator btop neofetch
